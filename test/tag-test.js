65e7e0d5833dba9d451f38d235f3034ca30e1e55
Alexander T <alexander.tarasyuk@outlook.com>
2016-03-22 17:03:05 +0200

    Removed namespaces

--- test/tag-test.js
+++ test/tag-test.js
@@ -12,29 +12,8 @@ suite('tag', function () {
     clear();
   });
 
-  test('should be error if first argument is not String or Array', function () {
-    assert.throw(tag, 'first argument must be Array or String and can not be empty');
-  });
-
-  test('should be available in local scope all elements which were specified in elements array', function () {
-    var t = tag(['div', 'span']);
-
-    assert.isFunction(t.div);
-    assert.isFunction(t.span);
-  });
-
-  test('should be available in global scope all elements which were specified in elements array', function () {
-    tag(['div', 'span'], true);
-
-    assert.isFunction(window.div);
-    assert.isFunction(window.span);
-  });
-
-  test('should be available in namespace in global scope all elements which were specified in elements array', function () {
-    tag(['div', 'span'], true, 'tags');
-
-    assert.isFunction(window.tags.div);
-    assert.isFunction(window.tags.span);
+  test('should be error if first argument is not String', function () {
+    assert.throw(tag, 'first argument must be String and can not be empty');
   });
 
   suite('#tag("tag name")', function () {
@@ -55,93 +34,6 @@ suite('tag', function () {
 
     test('should be created div with id="test" and class="test"', function () {
       var el = tag('div', {'id': 'test', 'class': 'test'}, 'test');
-
-      assert.strictEqual(el.className, 'test');
-      assert.strictEqual(el.getAttribute('id'), 'test');
-    });
-  });
-
-  suite('#tag(["tags"])', function () {
-    test('should be created new div', function () {
-      var t = tag(['div', 'span']);
-
-      container.appendChild(t.div());
-      assert.strictEqual(container.innerHTML, '<div></div>');
-    });
-
-    test('should be created new div with content "test"', function () {
-      var t = tag(['div', 'span']);
-
-      container.appendChild(t.div('test'));
-      assert.strictEqual(container.innerHTML, '<div>test</div>');
-    });
-
-    test('should be created new div with content "test" and child tag span which has content "child"', function () {
-      var t = tag(['div', 'span']);
-
-      container.appendChild(t.div(['test', t.span('child')]));
-      assert.strictEqual(container.innerHTML, '<div>test<span>child</span></div>');
-    });
-
-    test('should be created div with id="test" and class="test"', function () {
-      var t  = tag(['div', 'span']),
-          el = t.div({'id': 'test', 'class': 'test'}, 'test');
-
-      assert.strictEqual(el.className, 'test');
-      assert.strictEqual(el.getAttribute('id'), 'test');
-    });
-  });
-
-  suite('#tag(["tags"], true)', function () {
-    setup(function () {
-      tag(['div', 'span'], true);
-    })
-
-    test('should be created new div', function () {
-      container.appendChild(div());
-      assert.strictEqual(container.innerHTML, '<div></div>');
-    });
-
-    test('should be created new div with content "test"', function () {
-      container.appendChild(div('test'));
-      assert.strictEqual(container.innerHTML, '<div>test</div>');
-    });
-
-    test('should be created new div with content "test" and child tag span which has content "child"', function () {
-      container.appendChild(div(['test', span('child')]));
-      assert.strictEqual(container.innerHTML, '<div>test<span>child</span></div>');
-    });
-
-    test('should be created div with id="test" and class="test"', function () {
-      var el = div({'id': 'test', 'class': 'test'}, 'test')
-
-      assert.strictEqual(el.className, 'test');
-      assert.strictEqual(el.getAttribute('id'), 'test');
-    });
-  });
-
-  suite('#tag(["tags"], true, "tags")', function () {
-    setup(function () {
-      tag(['div', 'span'], true, 'tags');
-    })
-
-    test('should be created new div', function () {
-      container.appendChild(tags.div());
-      assert.strictEqual(container.innerHTML, '<div></div>');
-    });
-
-    test('should be created new div with content "test"', function () {
-      container.appendChild(tags.div('test'));
-      assert.strictEqual(container.innerHTML, '<div>test</div>');
-    });
-
-    test('should be created new div with content "test" and child tag span which has content "child"', function () {
-      container.appendChild(tags.div(['test', tags.span('child')]));
-      assert.strictEqual(container.innerHTML, '<div>test<span>child</span></div>');
-    });
-
-    test('should be created div with id="test" and class="test"', function () {
-      var el = tags.div({'id': 'test', 'class': 'test'}, 'test');
 
       assert.strictEqual(el.className, 'test');
       assert.strictEqual(el.getAttribute('id'), 'test');
