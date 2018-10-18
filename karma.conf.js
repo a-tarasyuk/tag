a4b621c3e309bddab836c2d5ff7b55f6bc903345
Alexander T <alexander.tarasyuk@outlook.com>
2018-10-18 14:37:00 +0300

    fix formatting

--- karma.conf.js
+++ karma.conf.js
@@ -1,29 +1,28 @@
 module.exports = function(config) {
-	config.set({
-	  files: [{
+  config.set({
+    files: [{
       pattern: 'test/**/*.spec.js', watched: false,
     }],
 
-	  preprocessors: {
-		  'test/**/*.spec.js': ['rollup']
+    preprocessors: {
+      'test/**/*.spec.js': ['rollup']
     },
 
     frameworks: ['jasmine'],
     autoWatch: true,
-	  reporters: ['spec'],
-	  logLevel: config.LOG_INFO,
+    reporters: ['spec'],
+    logLevel: config.LOG_INFO,
     basePath: '',
-	  colors: true,
+    colors: true,
     port: 9876,
 
     rollupPreprocessor: {
-			plugins: [require('rollup-plugin-buble')()],
-			output: { format: 'iife', name: 'tag', sourcemap: 'inline' }
+      plugins: [require('rollup-plugin-buble')()],
+      output: { format: 'iife', name: 'tag', sourcemap: 'inline' }
     },
 
     customLaunchers: {
       ChromeHeadlessNoSandbox: { base: 'ChromeHeadless', flags: ['--no-sandbox'] }
     },
-
   })
 };
